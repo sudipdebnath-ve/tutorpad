16cc9f2c04032bed618ed8e2833f9a8b7efa9796
RushitaGinoya <rnginoy@virtualemployee.com>
2024-05-01 16:29:03 +0530

    resolve conflicts

--- src/components/family-invoice/FamilyDetails.js
+++ src/components/family-invoice/FamilyDetails.js
@@ -209,43 +209,6 @@ const FamilyDetails = () => {
                     {/* {  Boolean(0) && <div> Hiiiii welcome</div>} */}
                     {Boolean(isAutoInvoicing) && (
                       <>
-<<<<<<< HEAD
-                        <div className="row">
-                          <div className="col-md-12">
-                            <label>
-                              <Icon icon={ic_receipt_outline} /> Next Invoice
-                              XYZ
-                            </label>
-                            <div style={{ lineHeight: "1.2", fontSize: 10 }}>
-                              <p>
-                                Invoice Date:{" "}
-                                {incoming_invoice?.invoice_create_date}
-                              </p>
-                              <p>
-                                Date Range:{" "}
-                                {incoming_invoice?.invoice_start_date} to{" "}
-                                {incoming_invoice.invoice_end_date}
-                              </p>
-                              <p>
-                                Invoice Date:{" "}
-                                {incoming_invoice?.invoice_create_date}
-                              </p>
-                              <p>
-                                Total Due:{" "}
-                                <span
-                                  style={{
-                                    background: "red",
-                                    padding: "2px 5px",
-                                    color: "white",
-                                  }}
-                                >
-                                  ₹{incoming_invoice?.amount}.00 balance owing
-                                </span>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-=======
                     <div className="row">
                         <div className="col-md-12">
                           <label><Icon icon={ic_receipt_outline} /> Next Invoice XYZ</label>
@@ -257,7 +220,6 @@ const FamilyDetails = () => {
                           </div>
                         </div>
                       </div>
->>>>>>> 00129a11d28ffd03277a288cb4afc9d6ac23c069
                         <div className="row">
                           <div className="col-md-12">
                             <label>
