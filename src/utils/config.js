727715c9e43fe8e7f5b2203d567bbfb79998ae39
RushitaGinoya <rnginoy@virtualemployee.com>
2024-05-07 17:25:43 +0530

    updated new api urls

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://13.233.254.223/tutorpad/api/auth/";
-export const NON_LOGGED_IN_API_URL = "http://13.233.254.223/tutorpad/api/";
+export const API_URL = "https://test.tutorpad.co/webservices/api/auth/";
+export const NON_LOGGED_IN_API_URL = "https://test.tutorpad.co/webservices/api/";
