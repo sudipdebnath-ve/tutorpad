ae332f02d129d1a198b91bf6710770606687e5c1
RushitaGinoya <rnginoy@virtualemployee.com>
2024-05-24 19:23:29 +0530

    call reset-pass api's4

--- src/components/users/students/ResetPassword.js
+++ src/components/users/students/ResetPassword.js
@@ -1,16 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import bgimage  from "../../../assets/images/bg.jpg";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { NON_LOGGED_IN_API_URL } from "../../../utils/config.js";
 import axios from "axios";
-import { useUserDataContext } from "../../../contextApi/userDataContext.js";
-import LanguageOption from "../../LanguageOption.js";
 import { useTranslation } from "react-i18next";
 // import i18next from "i18next";
-import { useTokenStorage, checkAuthAndRedirect } from '../../../utils/helper.js';
-import { getDomainName, passwordSetupPortal } from "../../../services/loginService.js";
 import "./style.css"
 
 const ResetPasswordSetup = () => {
@@ -18,47 +13,101 @@ const ResetPasswordSetup = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const location = useLocation();
-  const [error, setError] = useState({});
+  const [getError, setError] = useState({});
   const [portal, setPortal] = useState("");
   const [password, setPassword] = useState("");
   const [c_password, setC_password] = useState("");
   const [key, setKey] = useState("");
+  const [getAllow, setAllow] = useState(false);
 
-
-//   const multiLangHandler = (e) => {
-//     i18next.changeLanguage(e.target.value);
-//     console.log("change language-----------");
-//   };
-   const resetPassword = async () =>{
-
-    if(userdetails.password !== userdetails.rpassword){
-
+  const handleSubmit = async () => {
+    if (password !== c_password) {
+      setError("Passwords do not match.");
+      return;
     }
+    const loginApi = axios.create({
+      baseURL: NON_LOGGED_IN_API_URL,
+    });
+    const data = {
+      key: key,
+      portal: portal,
+      password: password,
+      c_password: c_password,
+    };
+    loginApi.post ("student/setup-password", data).then((response) =>{
+      console.log('response : ',response);
+      if (response && response.data.success) {
+        toast.success(response.data.message, {
+          position: toast.POSITION.TOP_CENTER,
+        });
+        navigate('/signin');  
+      }
+    })
+    .catch((error) => {
+      if (error.response && error.response.data) {
+        setError(error.response.data.data.password);
+        return;
+      }
+    });
+  }
 
-   }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "password") {
+      setPassword(value);
+    } else if (name === "rpassword") {
+      setC_password(value);
+    }
+  }
 
+  const verifyKey = async (data) => {
+    try {
+      const loginApi = axios.create({
+        baseURL: NON_LOGGED_IN_API_URL,
+      });
+      loginApi.post ("student/verify/password-setup-portal", data).then((response) =>{
+        console.log('response.data.success : ',response.data.success);
+        if (response && response.data.success) {
+          console.log('gsdsd : ', response.data.success);
+            setAllow(true);
+        }
+      })
+      .catch((error) => {
+        if (error.response && error.response.data) {
+          setError(error.response.data.message);
+          return;
+          } else {
+          return "An unknown error occurred.";
+        }
+      });
+    } catch (error) {
+      console.error("An error occurred:", error);
+      return false;
+    }
+  };
 
   useEffect(() => {
     // Extract the key from the URL
     const queryParams = new URLSearchParams(location.search);
     const key = queryParams.get('key');
     const portal = window.location.hostname;
+    setKey(key);
+    setPortal(portal);
     if (key) {
-        data: {
-            email: userdetails.email,
-            password: userdetails.password,
-            portal: portal,
-          }, 
-        passwordSetupPortal()
+      const data = {
+        key: key,
+        portal: portal,
+      }; 
+      verifyKey(data);
       }
-
-  }, [location]);
+  }, []);
 
   return (
     
     <div className="d-md-flex justify-content-center align-items-center h-100 primary-bg">
       <ToastContainer />
         <div className="contents">
+          { (getAllow) ?
           <div className="container">
             <div className="row align-items-center justify-content-center">
               <div className="col-md-12">
@@ -85,13 +134,11 @@ const ResetPasswordSetup = () => {
                         className="form-control"
                         placeholder={t("New Password")}
                         name="password"
-                        // onChange={handleChange}
+                        onChange={handleChange}
                         required
                       />
                     </div>
-                    <small style={{ color: "red" }}>
-                      {error?.message?.length ? error.message : <></>}
-                    </small>
+                    
 
                     <div className="form-group last mb-3">
                     <input
@@ -99,13 +146,14 @@ const ResetPasswordSetup = () => {
                       className="form-control"
                       placeholder={t("Confirm Password")}
                       name="rpassword"
-                    //   onChange={handleChange}
+                      onChange={handleChange}
                       required
                     />
                     <small style={{ color: "red" }}>
-                      {error?.c_password?.length ? error.c_password[0] : <></>}
+                      {getError?.length ? getError : <></>}
                     </small>
                   </div>
+                  
 
                   <div className="form-group  last mb-5">
                     <div className="d-sm-flex align-items-center justify-content-between">
@@ -125,14 +173,14 @@ const ResetPasswordSetup = () => {
                       </label>
                     </div>
                     <small style={{ color: "red" }}>
-                      {error?.terms?.length ? error.terms[0] : <></>}
+                      {getError?.terms?.length ? getError.terms[0] : <></>}
                     </small>
                   </div>
                     <input
                       type="button"
                       value="Save Password"
                       className="btn btn-block btn-primary save-password"
-                    //   onClick={handleSubmit}
+                      onClick={handleSubmit}
                     />
                   </form>
                   <br></br>
@@ -141,9 +189,19 @@ const ResetPasswordSetup = () => {
               </div>
             </div>
           </div>
+          : 
+          <div className="container">
+            <div className="row align-items-center justify-content-center">
+              <div className="col-md-12">
+                <small style={{ color: "red" }}>
+                  {getError?.length ? getError : 'Loding ...'}
+                </small> 
+              </div>
+            </div>
+          </div>
+          }
         </div>
     </div>
-    
   );
 };
 
